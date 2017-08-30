685903de6fd9afd4930d4b18cc71223fe7a75390
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2017-08-30 16:27:14 -0700

    initial barebones commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Amplified Developer Challenge
+
+Implementation by Jyotsna Prakash
+
+
+## Build and Running from Source
+   ``` bash
+   git clone https://github.com/moonlightdrive/amplified-challenge.git
+   cd amplified-challenge
+   cabal sandbox init
+   cabal install --only-dependencies
+   cabal configure
+   cabal build
+   ./dist/build/amplified-challenge/amplified-challenge
+   ```
+
+The webserver should now be running on `localhost:3000`. Try visiting [localhost:3000/phone?input=25](localhost:3000/phone?input=25). Or with `curl`,
+   ``` bash
+   curl -v -X GET localhost:3000/phone?input=25
+   ```
---
+++ src/Dialpad.hs
@@ -0,0 +1,43 @@
+module Dialpad
+--  (digitsFromString, telephoneWords)
+where
+
+import Control.Monad
+import Data.Char(isNumber, digitToInt)
+
+
+data PhoneDigit = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine
+  deriving (Show, Eq)
+
+-- too rethink types?
+telephoneMapping :: PhoneDigit -> [Char]
+telephoneMapping Zero  = ""
+telephoneMapping One   = ""
+telephoneMapping Two   = "abc"
+telephoneMapping Three = "def"
+telephoneMapping Four  = "ghi"
+telephoneMapping Five  = "jkl"
+telephoneMapping Six   = "mno"
+telephoneMapping Seven = "pqrs"
+telephoneMapping Eight = "tuv"
+telephoneMapping Nine  = "wxyz"
+
+hasMapping :: PhoneDigit -> Bool
+hasMapping digit =
+  case digit of
+    Zero      -> False
+    One       -> False
+    otherwise -> True
+
+-- TODO rename this also make it more legible
+digitsFromString :: String -> Maybe [PhoneDigit]
+digitsFromString =
+  let buttons = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+  in mapM (\c -> do { guard (isNumber c); return (buttons !!  digitToInt c) })
+
+
+telephoneWords :: [PhoneDigit] -> [String]
+telephoneWords = sequence . fmap telephoneMapping
+
+
+-- TODO what difference do One, Zero make?
