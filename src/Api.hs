d0d5ec944ab7f59bab63fd8d12b15448e92997ad
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2017-08-30 22:08:56 -0700

    create api module to handle http requests

---
+++ src/Api.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+module Api (app) where
+
+import Data.Aeson
+import GHC.Generics
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+import Servant
+
+import Dialpad
+
+
+type DialpadAPI = "phone" :> QueryParam "input" String :> Get '[JSON] [String]
+
+errNoInput, errNotPhoneSeq :: ServantErr
+errNoInput = ServantErr 400 "Query string 'input' not found" "" []
+errNotPhoneSeq = ServantErr 400 "Query string 'input' must be a sequence of digits" "" []
+
+phone :: Maybe String -> Handler [String]
+phone Nothing = throwError errNoInput
+phone (Just input) =
+  let possibleWords = digitsFromString input >>= return . telephoneWords
+  in case possibleWords of
+    Nothing -> throwError errNotPhoneSeq
+    Just ws -> return ws
+
+app :: Application
+app = serve (Proxy :: Proxy DialpadAPI) server
+
+server :: Server DialpadAPI
+server = phone
--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,6 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators #-}
+import Network.Wai.Handler.Warp(run)
 
-import Data.Aeson
-import GHC.Generics
-import Network.Wai (Application)
-import Network.Wai.Handler.Warp (run)
-import Servant
-
-import Dialpad
-
-
-type DialpadAPI = "phone" :> QueryParam "input" String :> Get '[JSON] [String]
-
-errNoInput, errNotPhoneSeq :: ServantErr
-errNoInput = ServantErr 400 "Query string 'input' not found" "" []
-errNotPhoneSeq = ServantErr 400 "Query string 'input' must be a sequence of digits" "" []
-
-phone :: Maybe String -> Handler [String]
-phone Nothing = throwError errNoInput
-phone (Just input) =
-  let possibleWords = digitsFromString input >>= return . telephoneWords
-  in case possibleWords of
-    Nothing -> throwError errNotPhoneSeq
-    Just ws -> return ws
-
-app :: Application
-app = serve (Proxy :: Proxy DialpadAPI) server
-
-server :: Server DialpadAPI
-server = phone
+import Api(app)
 
 main :: IO ()
-main = run 3000 app
+main = run 3000 Api.app
